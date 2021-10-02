82d9b42d389c52d0d8a3c89a9cefaf0fd3a2245a
Xbetty <betty.xiong@pdabc.com>
2021-10-02 14:57:16 +0800

    feat: 打包图片资源

--- Webpack/webpack1/config/webpack.config.js
+++ Webpack/webpack1/config/webpack.config.js
@@ -48,7 +48,34 @@ module.exports = {
                     // 将less文件编译成css文件，需要下载less-loader和less
                     'less-loader'
                 ]
-            }
+            },
+            {
+                // 处理图片资源 
+                // 问题：默认处理不了html中的img图片
+                test: /\.(jpg|png|gif)$/,
+                // 使用一个loader
+                // 下载url-loader file-loader（url-loader依赖file-loader）
+                loader: 'url-loader',
+                // 配置
+                options: {
+                    // 图片大小小于8kb，就会被base64处理
+                    // base64的优点：减少请求数量（减轻服务器压力）；缺点：图片体积会更大（文件请求速度更慢）
+                    limit: 8 * 1024,
+                    // 问题：因为url-loader默认使用es6模块解析，而html-loader引入图片是commonjs
+                    // 解析时会出问题：[object Module]
+                    // 解决：关闭url-loader的es6模块化，使用commonjs解析
+                    esModule: false,
+                    // 给图片重命名
+                    // [hash:10]取图片的hash的前10位
+                    // [ext]取文件原来扩展名
+                    name: '[hash:10].[ext]'
+                }
+            },
+            {
+                test: /\.html$/,
+                // 处理html文件的img图片（负责引入img，从而能被url-loader进行处理）
+                loader: 'html-loader'
+            },
         ]
     },
     // plugins的配置：1. 下载 2. 引入 3. 使用 
